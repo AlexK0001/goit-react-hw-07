aa9165858a2880d3756900346af52ab5b0613df6
Alexandr <alexandr.kopyletz@gmail.com>
2024-09-20 23:09:46 +0300

    20.09/3

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -10,6 +10,7 @@ export const fetchContacts = createAsyncThunk(
         const res = await axios.get("/contacts");
         return res.data;
       } catch (error) {
+        console.error(error);
         return thunkAPI.rejectWithValue();
       }
     }
@@ -22,6 +23,7 @@ export const fetchContacts = createAsyncThunk(
         const res = await axios.post("/contacts", newContact);
         return res.data;
       } catch (error) {
+        console.error(error);
         return thunkAPI.rejectWithValue();
       }
     }
@@ -34,6 +36,7 @@ export const fetchContacts = createAsyncThunk(
         const res = await axios.delete(`/contacts/${contactsId}`);
         return res.data;
       } catch (error) {
+        console.error(error);
         return thunkAPI.rejectWithValue();
       }
     }
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -15,7 +15,7 @@ export const selectFilter = (state) => state.filter;
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectFilter],
   (contacts, filter) => contacts?.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
+    contact.name?.toLowerCase().includes(filter?.toLowerCase())
   )
 );
 
